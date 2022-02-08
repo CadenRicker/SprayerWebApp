1528e26acc2bec7c20f12f7e9b8721bbd6657288
Caden Ricker <cricker@eou.edu>
2022-02-08 08:00:44 -0700

    Update SprayerTabeles.sql

--- SprayerApplication/SQLtables/SprayerTabeles.sql
+++ SprayerApplication/SQLtables/SprayerTabeles.sql
@@ -1,22 +1,21 @@
 USE sprayCatalog;
-CREATE TABLE Crop(
+CREATE TABLE plant (
     id int,
     name varchar(20),
     primary key (name)
 );
-CREATE TABLE weed(
-    id int,
-    name varchar(20),
-    primary key (name)
-);
+
 CREATE TABLE spray(
     id int,
     name varchar(20),
+    Decimal(13,2) price,    
     primary key (name)
 );
 CREATE TABLE SpraySafe(
     int sprayId,
-    int cropId,
+    int plantId,
+    int rating,
+    Decimal(15,4) concentration,
     foreign key sprayId references spray sprayId,
     primary key (sprayId, cropId)
 )
